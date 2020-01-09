3c20acc6f9290a7ce5a248ccf4577eb22c5c8e61
Michael Capobianco <mccapobianco@wpi.edu>
2020-01-09 00:31:36 -0500

    add comments and remove hardcoded paths

--- scrape_hit_prob.py
+++ scrape_hit_prob.py
@@ -7,15 +7,21 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import time
 import pandas as pd
 
-binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+firefox_path = input("Enter path to firefox.exe: ") #mine is 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+binary = FirefoxBinary(firefox_path)
 cap = DesiredCapabilities().FIREFOX
 cap["marionette"] = True
-driver = webdriver.Firefox(capabilities=cap, executable_path="C:\\Program Files\\GeckoDriver\\geckodriver.exe", firefox_binary=binary)
+geckodriver_path = input('Enter path to geckodriver.exe: ') #mine is "C:\\Program Files\\GeckoDriver\\geckodriver.exe"
+driver = webdriver.Firefox(capabilities=cap, executable_path=geckodriver_path, firefox_binary=binary)
 url = 'https://baseballsavant.mlb.com/statcast_hit_probability'
+#open baseball savant using selenium firefox webdriver
 driver.get(url)
+#sleep 5 seconds to make sure everything loads (better way to do this?)
 time.sleep(5)
+
 elements = driver.find_elements_by_css_selector('.default-table-row')
-for element in elements:	
+for element in elements:
+	#click every row to expand it
 	subel = element.find_element_by_tag_name('td')
 	subel.click()
 html = driver.page_source
@@ -25,19 +31,22 @@ mydivs = mydivs.find('table')
 mydivs = mydivs.find('tbody')
 mydivs = mydivs.findAll('tr')
 d = {}
+#mydivs contains rows
 for div in mydivs:
 	divid = div.get('id')
 	if divid is None:
 		pass
+	#only care about row if contains 'data_' and '-tr_' substrings (maybe use reg. expr. instead?)
 	elif 'data_' in divid and '-tr_' in divid:
+		#exit velo value will be between '_' and '-' (maybe use reg. expr. instead?)
 		velo = divid.split('_')[1].split('-')[0]
 		angle_data = div.findAll('td')
-		angle = angle_data[0].find('span').string
-		bbe = angle_data[2].find('span').string
-		_1B = angle_data[5].find('span').string
-		_2B = angle_data[6].find('span').string
-		_3B = angle_data[7].find('span').string
-		HR = angle_data[8].find('span').string
+		angle = angle_data[0].find('span').string #launch angle
+		bbe = angle_data[2].find('span').string #batted balls
+		_1B = angle_data[5].find('span').string #singles
+		_2B = angle_data[6].find('span').string #doubles
+		_3B = angle_data[7].find('span').string #triples
+		HR = angle_data[8].find('span').string #home runs
 		if velo not in d.keys():
 			d[velo] = {}
 		d[velo][angle] = (bbe, _1B, _2B, _3B, HR)
