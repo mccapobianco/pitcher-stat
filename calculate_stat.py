87185b07ace7539d4a614fc755997e49f6232af5
Michael Capobianco <mccapobianco@wpi.edu>
2020-01-12 01:27:31 -0500

    remove commented out code and convert 0.1 IP to 1/3

--- calculate_stat.py
+++ calculate_stat.py
@@ -14,14 +14,6 @@ import re
 import sys
 import time
 
-
-def get_pitcher_stats(player_id, year):
-	""" returns a DataFrame containing data for all pitches that a pitcher threw in a year"""
-	sys.stdout = open(os.devnull, "w") #prevent statcast_pitcher from printing to console
-	stats = statcast_pitcher(str(year)+'-01-01', last_day(year), player_id) 
-	sys.stdout = sys.__stdout__ #allow printing to console
-	return stats
-
 def last_day(year, team='NYY'):
 	"""returns the last day of the regular season"""
 	last_day_str=list(schedule_and_record(year, team)['Date'])[-1]
@@ -45,39 +37,11 @@ def format_player_name(string):
 
 def get_player_table():
 	"""merge id_table and pitcher_list"""
-	#TODO output to csv
 	ids = get_id_table()
 	pitcher_list = get_pitcher_list()
 	player_table = pd.merge(pitcher_list, ids, how='inner', left_on=['Name', 'Tm'], right_on=['bref_id', 'mlb_team'])
 	player_table = player_table.loc[player_table['Name'] != 'LgAvg per 180 IP'] #remove extraneous row
 	return player_table[['mlb_name','mlb_id']]
-
-# def get_player_info(last_name, first_name):
-# 	player_info = playerid_lookup(last_name, first_name)
-# 	if len(player_info) > 1:
-# 		s = 'Multiple Results Found. Please choose one:\n'
-# 		i=0
-# 		for index, player in player_info.iterrows():
-# 			start = player['mlb_played_first']
-# 			try:
-# 				start = int(start)
-# 			except:
-# 				pass
-# 			end = player['mlb_played_last']
-# 			try:
-# 				end = int(end)
-# 			except:
-# 				pass
-# 			s += ('({}) '.format(i) + '{} {} played from {} to {}'.format(player['name_first'], player['name_last'], start, end))
-# 			s += '\n'
-# 			i+=1
-# 		selection = input(s+'Choose (enter a number between 0 and {}): '.format(len(player_info)-1))
-# 		return player_info.loc[int(selection)]
-# 	else:
-# 		print('Found player.')
-# 		return player_info
-
-
 
 def load_re_matrix():
 	"""run expectation matrix"""
@@ -150,12 +114,6 @@ def get_end_re(pitch_data, re_matrix, hp_matrix, model):
 		out_re = get_re_from_mat(re_matrix, o, r1, r2, r3) + runs
 	velo, angle = get_velo_and_angle(pitch_data)
 	hp_data = get_hp_data_from_nn(model, velo, angle)
-	#hp_data[i] => i=0: occurences | i=1: singles | i=2: doubles | i=3: triples | i=4: homeruns 
-	# single_rate = hp_data[1] / hp_data[0]
-	# double_rate = hp_data[2] / hp_data[0]
-	# triple_rate = hp_data[3] / hp_data[0]
-	# homerun_rate = hp_data[4] / hp_data[0]
-	# out_rate = 1 - sum((single_rate, double_rate, triple_rate, homerun_rate))
 	#TODO phase out hp_matrix
 	out_rate, single_rate, double_rate, triple_rate, homerun_rate = hp_data
 	#expected value: E(X) = ∑(X * P(X))
@@ -220,65 +178,13 @@ def walk(re_matrix, pitch_data):
 	after = get_re_from_mat(re_matrix, outs, True, runner1, runner1 and runner2)+int(runner1 and runner2 and runner3)
 	return before-after
 
-def season_change_in_re(player_id, year, re_matrix, hp_matrix, model):
-	"""calculates the expected change in run expectency for the entire season"""
-	df = get_pitcher_stats(player_id, year)
-	re = 0
-	for index, row in df.iterrows():
-		#if ball is put in play
-		#TODO filter out before iteration
-		if type(row['type']) == 'X':
-			re += change_in_re(row, re_matrix, hp_matrix, model)
-		#skip if event is a caught stealing
-		#TODO filter out before iteration
-		# elif row['events'] in ['caught_stealing_2b', 'caught_stealing_3b']:
-		# 	continue
-		#if strikeout, add strikeout change in run expectancy
-		#TODO would this work? : elif 'strikeout' in row['events']
-		elif row['events'] in ['strikeout', 'strikeout_double_play']:
-			re += strikeout(re_matrix, row)
-		#if bb or hbp, add walk change in run expectancy 
-		elif row['events'] in ['walk', 'hit_by_pitch']:
-			re += walk(re_matrix, row)
-		#if pitch does not matter
-		else:
-			continue
-	return re
-
-def calculate(player_id, year, re_matrix, hp_matrix, model):
-	"""calculate stat for one player"""
-	
-	return season_change_in_re(player_id, year, re_matrix, hp_matrix, model)
-
-def calculate_all(year, player_table, progress=0):
-	"""calculate stat for all players"""
-	table = player_table.copy()
-	row_count = len(table)
-	re_matrix = load_re_matrix()
-	hp_matrix = load_hp_matrix()
-	model = load_model()
-	bar = progressbar.ProgressBar(maxval=row_count, \
-		widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage(), ' | ', progressbar.ETA()])
-	bar.start()
-	stats = []
-	for index, row in list(table.iterrows()):
-		calc = calculate(row['mlb_id'], year, re_matrix, hp_matrix, model)
-		stats.append(calc)
-		bar.update(index+1)
-	bar.update(row_count)
-	table['Value'] = stats
-	table.rename(columns={"mlb_name":"Name"}, inplace=True)
-	table.sort_values(by="Value", inplace=True, ascending=False)
-	table.index = np.arange(1,len(table)+1) #start index at 1 instead of 0
-	return table
-
 def export_values(calc_table, filename='values.csv'):
 	"""export all players stats to a csv file"""
 	try:
 		with open(filename, 'w', newline='') as file:
 			file.write(calc_table.to_csv())
 	#if file is open, this error occurs. Try exporting again	
-	except (PermissionError):
+	except (PermissionError, FileNotFoundError):
 		x = input('Error exported values. Enter a filename to export to or hit Enter to try the same filename again.')
 		if not x:
 			x = filename
@@ -318,9 +224,10 @@ def calc2():
 	stats = pitching_stats.pitching_stats_bref(2019)
 	table = pd.merge(table, stats, left_on=['id'], right_on=['mlb_ID'])
 	table = table[['Name', 'Value', 'G', 'IP', "BF"]]
-	#TODO fix division (6.1 IP = 6+1/3 IP)
 	val_g = table['Value']/table['G']
-	val_ip = table['Value']/table['IP']
+	#convert every 0.1 innings pitched to 1/3 inning pitched
+	real_ips = [ip + (ip - int(ip)) * 10/3 for ip in table['IP']]
+	val_ip = table['Value']/real_ips
 	val_bf = table['Value']/table['BF']
 	table['Value/G'] = val_g
 	table['Value/IP'] = val_ip
@@ -330,23 +237,8 @@ def calc2():
 	table.index = np.arange(1,len(table)+1)
 	return table
 
-
-
-#TODO delete
-# name = input('Enter pitcher name: ')
-# year = input('Enter year: ')
-# names = name.split(' ', 1) if ' ' in name else name
-# re, error = calculate(names[1], names[0],year) if ' ' in name else calculate(names, None, year)
-# print(name+"'s", str(year), "value is {:0.4f} (".format(re)+str(error), 'data points excluded)')
-
-# start = time.time()
-# print("Calculating all pitchers' values. This should take several minutes")
-# calc_table = calculate_all(2019, get_player_table())
-# export_values(calc_table)
-# print('\nDone in {} seconds'.format(int(np.ceil(time.time()-start))))
-
 start = time.time()
 print("Calculating all pitchers' values. This should take several minutes")
 calc_table = calc2()
-export_values(calc_table, 'values2.csv')
+export_values(calc_table)
 print('\nDone in {} seconds'.format(int(np.ceil(time.time()-start))))
